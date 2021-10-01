adcabc673ca43540a3cba9320df3e9e30a5d0b13
SDog6 <90568016+SDog6@users.noreply.github.com>
2021-10-01 15:00:15 +0200

    added code according to the UML in the classes

---
+++ Backend/src/main/java/Backend/Interfaces/IManager.java
@@ -0,0 +1,15 @@
+package Backend.Interfaces;
+
+import java.util.ArrayList;
+
+public interface IManager {
+
+public boolean AddObject(Object a);
+
+public boolean RemoveObject(Object a);
+
+public ArrayList<Object> GetAllObjects();
+
+
+
+}
