54e9ce4d2afd956372cf99d30330700ac249e028
SDog6 <90568016+SDog6@users.noreply.github.com>
2021-10-04 10:25:36 +0200

    Some changes to backend

--- Backend/src/main/java/Backend/CrossynApp.java
+++ Backend/src/main/java/Backend/CrossynApp.java
@@ -1,6 +1,7 @@
 package Backend;
 import Backend.Classes.Trip;
 import Backend.Classes.TripEntry;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.tomcat.util.json.JSONParser;
 import org.json.JSONArray;
@@ -14,6 +15,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 
 @SpringBootApplication
@@ -21,21 +23,29 @@ public class CrossynApp {
 
     public static void main(String a[]) throws IOException {
 
-        BufferedReader bufReader = new BufferedReader(new FileReader("C:\\GITWORK\\test\\src\\main\\java\\Backend\\DataStream\\dataset1.txt"));
+        BufferedReader bufReader = new BufferedReader(new FileReader("C:\\GitStuff\\crossyn_project\\Backend\\src\\main\\java\\Backend\\DataStream\\dataset1.txt"));
         ArrayList<String> listOfLines = new ArrayList<>();
 
+        String k = "";
         String line = bufReader.readLine();
         while (line != null) {
-            listOfLines.add(line);
+            k = k+line;
             line = bufReader.readLine();
-
         }
         bufReader.close();
 
-        System.out.println(listOfLines.stream().count());
+        System.out.println(k);
+        ObjectMapper mapper = new ObjectMapper();
+
+//
+//        List<TripEntry> participantJsonList = mapper.readValue(k, new TypeReference<List<TripEntry>>(){});
+//        System.out.println(participantJsonList.stream().count());
+//
 
 
-//        for (String p: listOfLines) {
+//        ArrayList<TripEntry> pls = new ArrayList<>();
+//
+//       for (String p: listOfLines) {
 //            JSONObject plss = new JSONObject(p);
 //            pls.add(plss);
 //
@@ -45,22 +55,21 @@ public class CrossynApp {
 
 
 
-//
+
 //
 //        ArrayList<TripEntry> temp = new ArrayList<TripEntry>();
-//        BufferedReader br = new BufferedReader(new FileReader("C:\\GITWORK\\test\\src\\main\\java\\Backend\\DataStream\\dataset1.txt"));
-//
 //
 //        ObjectMapper mapper = new ObjectMapper();
 //
 //
 //        for (String b: listOfLines) {
+//            System.out.println(b);
 //            TripEntry tripEntry = mapper.readValue(b, TripEntry.class);
 //            temp.add(tripEntry);
 //            System.out.println(temp.stream().count());
 //        }
-//
-//
+
+
 
         /* Gson g = new Gson();
         for (String b : listOfLines) {
